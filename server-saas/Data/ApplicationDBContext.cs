3e272ff965a03534235c5228e0895aa2700a942b
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-15 17:46:36 +0530

    Connect DB connection

---
+++ server-saas/Data/ApplicationDBContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using server_saas.Models;
+
+namespace server_saas.Data
+{
+    public class ApplicationDBContext : IdentityDbContext<User>
+    {
+        public ApplicationDBContext(DbContextOptions dbContextOptions) : base(dbContextOptions) { }
+    }
+}
--- server-saas/Extentions/ClaimExtentions.cs
+++ server-saas/Extentions/ClaimExtentions.cs
@@ -1,6 +1,18 @@
-﻿namespace server_saas.Extentions
+﻿using System.Security.Claims;
+
+namespace server_saas.Extentions
 {
-    public class ClaimExtentions
+    public static class ClaimExtentions
     {
+        public static string GetUsername(this ClaimsPrincipal user)
+        {
+            if (user == null)
+            {
+                throw new ArgumentNullException(nameof(user), "ClaimPrincipal cannot be null");
+            }
+
+            var nameClaim = user.Claims.SingleOrDefault(x => x.Type.Equals("http://schemas.xmlsoap.org/ws/2005/05/identity/claims/givenname"));
+            return nameClaim?.Value;
+        }
     }
 }
