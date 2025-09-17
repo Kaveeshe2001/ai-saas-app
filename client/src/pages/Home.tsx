e972fba39ad158e15b48b5d34ea7b3c62c8dac12
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-17 15:58:31 +0530

    SignUp page

--- client/src/pages/Home.tsx
+++ client/src/pages/Home.tsx
@@ -1,10 +1,6 @@
-import Navbar from "../components/Navbar"
-
 const Home = () => {
   return (
-    <>
-      <Navbar />
-    </>
+    <div>Home</div>
   )
 }
 
--- client/src/routes/Routes.tsx
+++ client/src/routes/Routes.tsx
@@ -1,10 +1,17 @@
 import { createBrowserRouter } from "react-router-dom";
 import App from "../App";
+import Login from "../pages/Login/Login";
+import Signup from "../pages/Signup/Signup";
+import Home from "../pages/Home";
 
 export const router = createBrowserRouter([
     {
         path: '/',
         element: <App />,
-        children: []
+        children: [
+            { index: true, element: <Home /> },
+            { path: 'login', element: <Login /> },
+            { path: 'signup', element: <Signup /> },
+        ]
     }
 ]);
