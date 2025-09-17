46fdf1f9e03fcd86a62bbf49d7dd850f5cdbbd7a
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-17 12:27:10 +0530

    ProtectedRoutes and useAuth

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,33 +1,21 @@
-import { Route, Routes } from "react-router-dom"
-import Home from "./pages/Home"
-import Layout from "./pages/Layout"
-import Dashboard from "./pages/Dashboard"
-import WriteArticle from "./pages/WriteArticle"
-import BlogTitles from "./pages/BlogTitles"
-import GenerateImages from "./pages/GenerateImages"
-import RemoveBackground from "./pages/RemoveBackground"
-import RemoveObject from "./pages/RemoveObject"
-import ReviewResume from "./pages/ReviewResume"
-import Community from "./pages/Community"
+import { Outlet, useLocation } from "react-router-dom"
+import { UserProvider } from "./providers/UserProvider"
+import { ToastContainer } from "react-toastify"
+import Navbar from "./components/Shared/Navbar"
 
 const App = () => {
-  return (
-    <div>
-      <Routes>
-        <Route path='/' element={<Home />} />
-        <Route path='/ai' element={<Layout />}>
-          <Route index element={<Dashboard />} />
-          <Route path="write-article" element={<WriteArticle />} />
-          <Route path="blog-title" element={<BlogTitles />} />
-          <Route path="generate-images" element={<GenerateImages />} />
-          <Route path="remove-background" element={<RemoveBackground />} />
-          <Route path="remove-object" element={<RemoveObject />} />
-          <Route path="review-resume" element={<ReviewResume />} />
-          <Route path="community" element={<Community />} />
-        </Route>
-      </Routes>
-    </div>
-  )
-}
+  const location = useLocation();
+  const showNavbar = !['/login', '/signup'].includes(location.pathname);
 
-export default App
+  return (
+    <>
+      <UserProvider>
+          {showNavbar && <Navbar />}
+          <Outlet />
+          <ToastContainer />
+      </UserProvider>
+    </>
+  );
+};
+
+export default App;
