d1818f39adf8e2567faa4bf819f8bd4c4af27e0d
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-23 15:25:47 +0530

    Background Removal repository

---
+++ server-saas/Interfaces/IBEditedImageRepository.cs
@@ -0,0 +1,11 @@
+﻿using server_saas.Models;
+
+namespace server_saas.Interfaces
+{
+    public interface IBEditedImageRepository
+    {
+        Task<BEditedImage> CrerateAsync(BEditedImage image);
+        Task<List<BEditedImage>> GetAllByUserIdAsync(string userId);
+        Task<BEditedImage?> GetByIdAsync(int id);
+    }
+}
---
+++ server-saas/Repository/BEditedImageRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server_saas.Data;
+using server_saas.Interfaces;
+using server_saas.Models;
+
+namespace server_saas.Repository
+{
+    public class BEditedImageRepository : IBEditedImageRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public BEditedImageRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<BEditedImage> CreateAsync(BEditedImage image)
+        {
+            await _context.BeditedImages.AddAsync(image);
+            await _context.SaveChangesAsync();
+            return image;
+        }
+
+        public async Task<List<BEditedImage>> GetAllByUserIdAsync(string userId)
+        {
+            return await _context.BeditedImages
+                .Where(x => x.UserId == userId)
+                .OrderByDescending(x => x.CreatedAt)
+                .ToListAsync();
+        }
+
+        public async Task<BEditedImage?> GetByIdAsync(int id)
+        {
+            return await _context.BeditedImages.FirstOrDefaultAsync(x => x.Id == id);
+        } 
+    }
+}
