1877eaaa1f2da39f8e7c5be875e216fab0e47243
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-23 15:18:01 +0530

    BEditedImage Mapper

---
+++ server-saas/Mappers/BEditedImageMappers.cs
@@ -0,0 +1,19 @@
+﻿using server_saas.Dto.BackgroundRemove;
+using server_saas.Models;
+
+namespace server_saas.Mappers
+{
+    public static class BEditedImageMappers
+    {
+        public static BEditedImageDto ToBEditedImageDto(BEditedImage bEditedImage)
+        {
+            return new BEditedImageDto
+            {
+                Id = bEditedImage.Id,
+                ProcessedImageUrl = bEditedImage.ProcessedImageUrl,
+                OperationType = bEditedImage.OperationType,
+                CreatedAt = bEditedImage.CreatedAt,
+            };
+        }
+    }
+}
