b8a4c88ede70b3fa0a6615aa86abfd3301d2079e
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-26 18:14:09 +0530

    Migration update

---
+++ server-saas/Migrations/20250926121655_Contact.cs
@@ -0,0 +1,79 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace server_saas.Migrations
+{
+    /// <inheritdoc />
+    public partial class Contact : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Articles",
+                table: "Articles");
+
+            migrationBuilder.RenameTable(
+                name: "Articles",
+                newName: "Contact");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact",
+                column: "Id");
+
+            migrationBuilder.CreateTable(
+                name: "Contactus",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Name = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Subject = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    Message = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Contactus", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Feedback",
+                columns: table => new
+                {
+                    Id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    Email = table.Column<string>(type: "nvarchar(max)", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Feedback", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Contactus");
+
+            migrationBuilder.DropTable(
+                name: "Feedback");
+
+            migrationBuilder.DropPrimaryKey(
+                name: "PK_Contact",
+                table: "Contact");
+
+            migrationBuilder.RenameTable(
+                name: "Contact",
+                newName: "Articles");
+
+            migrationBuilder.AddPrimaryKey(
+                name: "PK_Articles",
+                table: "Articles",
+                column: "Id");
+        }
+    }
+}
--- server-saas/Models/ContactUs.cs
+++ server-saas/Models/ContactUs.cs
@@ -1,6 +1,17 @@
-﻿namespace server_saas.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace server_saas.Models
 {
     public class ContactUs
     {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Name { get; set; } = string.Empty;
+        [Required]
+        public string Email { get; set; } = string.Empty;
+        [Required]
+        public string Subject { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
     }
 }
