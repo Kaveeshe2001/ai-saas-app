05a83bd1e2155fbc271127568fb00eb5795febc6
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-26 16:22:47 +0530

    FeedbackMappers

---
+++ server-saas/Mappers/FeedbackMappers.cs
@@ -0,0 +1,25 @@
+﻿using server_saas.Dto.Feedback;
+using server_saas.Models;
+
+namespace server_saas.Mappers
+{
+    public static class FeedbackMappers
+    {
+        public static FeedbackDto ToFeedbackDto(this Feedback feedbackModel) 
+        {
+            return new FeedbackDto
+            {
+                Id = feedbackModel.Id,
+                Email = feedbackModel.Email,
+            };
+        }
+
+        public static Feedback ToFeedbackFromCreate(this CreateFeedbackDto createFeedbackModel)
+        {
+            return new Feedback
+            {
+                Email = createFeedbackModel.Email,
+            };
+        } 
+    }
+}
