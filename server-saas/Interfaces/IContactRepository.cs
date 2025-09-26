a3331b5d0ff986eb95394811ebf518413f52e828
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-26 15:47:39 +0530

    COntact Repositories

---
+++ server-saas/Interfaces/IContactRepository.cs
@@ -0,0 +1,11 @@
+﻿using server_saas.Models;
+
+namespace server_saas.Interfaces
+{
+    public interface IContactRepository
+    {
+        Task<List<ContactUs>> GetAllAsync();
+        Task<ContactUs?> GetByIdAsync(int id);
+        Task<ContactUs> CreateAsync(ContactUs contactModel);
+    }
+}
---
+++ server-saas/Repository/ContactRepository.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.EntityFrameworkCore;
+using server_saas.Data;
+using server_saas.Interfaces;
+using server_saas.Models;
+
+namespace server_saas.Repository
+{
+    public class ContactRepository : IContactRepository
+    {
+        private readonly ApplicationDBContext _context;
+
+        public ContactRepository(ApplicationDBContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<ContactUs>> GetAllAsync()
+        {
+            return await _context.Contactus.ToListAsync();
+        }
+
+        public async Task<ContactUs> CreateAsync(ContactUs contactModel)
+        {
+            await _context.Contactus.AddAsync(contactModel);
+            await _context.SaveChangesAsync();
+            return contactModel;
+        }
+
+        public async Task<ContactUs?> GetByIdAsync(int id)
+        {
+            return await _context.Contactus.FirstOrDefaultAsync(x => x.Id == id);
+        }
+    }
+}
