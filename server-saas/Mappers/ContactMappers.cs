eea749fcc63ecfd051f568d53dff7539c7abd5ae
Kaveesha <kaveeshewaduge111@gmail.com>
2025-09-26 15:50:47 +0530

    COntactMappers

---
+++ server-saas/Mappers/ContactMappers.cs
@@ -0,0 +1,31 @@
+﻿using server_saas.Dto.ContactUs;
+using server_saas.Models;
+
+namespace server_saas.Mappers
+{
+    public static class ContactMappers
+    {
+        public static ContactDto ToContactDto(this ContactUs contactModel)
+        {
+            return new ContactDto
+            {
+                Id = contactModel.Id,
+                Name = contactModel.Name,
+                Email = contactModel.Email,
+                Subject = contactModel.Subject,
+                Message = contactModel.Message,
+            };
+        }
+
+        public async ContactUs ToContactFromCreate(this CreateContactDto createContactDto)
+        {
+            return new ContactUs
+            {
+                Name = createContactDto.Name,
+                Email = createContactDto.Email,
+                Subject = createContactDto.Subject,
+                Message = createContactDto.Message,
+            };
+        }
+    }
+}
